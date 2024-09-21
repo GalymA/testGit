b154f6d41b2b66d8567f571e6f891990b7444ef8
bricks <gbek0707@gmail.com>
2024-09-21 09:37:12 +0500

    added function multy and print

--- moduls/first.py
+++ moduls/first.py
@@ -2,13 +2,15 @@ def plus(a, b):
     return a + b
 
 
-text = 'new text'
-
-
 def minus(a, b):
     return a - b
+
+
+def multy(a, b):
+    return a * b
 
 
 if __name__ == '__main__':
     print(plus(3, 3))
     print(minus(10, 3))
+    print(multy(10, 3))
